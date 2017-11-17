493d55f11d5a25ef0890cc074810c42c54b33581
Simon Oulevay <simon.oulevay@gmail.com>
2017-11-17 13:09:56 +0100

    Move app.js to server directory & clean up bin/www

--- server/app.js
+++ server/app.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 
-var app = express();
+const config = require('../config');
+
+const app = express();
+
+app.set('env', config.env);
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
