3687ec9d8ed186d1047b9cc90683173d357b54b3
Simon Oulevay <simon.oulevay@gmail.com>
2017-12-01 17:02:35 +0100

    Simplify and document node scripts TG-103
    The scripts have been simplified with async/await and documented.
    
    `jsdoc.json` has been modified to include the `scripts` directory in the
    generated source code documentation. The `docs:src:watch` task has been
    updated to also watch for changes in the `scripts` directory.

--- scripts/anonymize.js
+++ scripts/anonymize.js
@@ -6,6 +6,16 @@ if (config.env != 'development') {
   throw new Error('This script should only be run during development');
 }
 
+/**
+ * Script to anonymize production data.
+ *
+ * This script changes the passwords of all user accounts to "test".
+ *
+ *     $> npm run anonymize
+ *
+ * @class
+ * @memberof module:scripts
+ */
 class AnonymizeScript extends Script {
   async run() {
     const users = await new User().fetchAll();
--- scripts/create-admin.js
+++ scripts/create-admin.js
@@ -2,18 +2,23 @@ const Script = require('./script');
 const User = require('../server/models/user');
 const valib = require('valib');
 
+/**
+ * Script to create an admin user.
+ *
+ * The `$ADMIN_EMAIL` and `$ADMIN_PASSWORD` environment variables must be set.
+ *
+ * You may use this script to create an admin user when first deploying the
+ * application in production.
+ *
+ *     $> ADMIN_EMAIL=admin@example.com ADMIN_PASSWORD=test npm run create-admin
+ *
+ * @class
+ * @memberof module:scripts
+ */
 class CreateAdminScript extends Script {
-  run() {
-    return Promise
-      .resolve()
-      .then(() => this.validateInput())
-      .then(() => this.loadExistingAdmin())
-      .then(() => this.createAdmin());
-  }
+  async run() {
 
-  validateInput() {
-
-    // Take input from environment variables
+    // Take input from the environment variables.
     this.adminEmail = process.env.ADMIN_EMAIL;
     this.adminPassword = process.env.ADMIN_PASSWORD;
 
@@ -22,29 +27,21 @@ class CreateAdminScript extends Script {
     } else if (!this.adminPassword || !this.adminPassword.trim().length) {
       throw new Error('$ADMIN_PASSWORD is required and cannot be blank');
     }
-  }
 
-  loadExistingAdmin() {
-    return new User({
+    // Check if an admin with that e-mail already exists.
+    this.existingAdmin = await new User({
       email: this.adminEmail
-    }).fetch().then(admin => {
-      this.existingAdmin = admin;
-    });
-  }
+    }).fetch();
 
-  createAdmin() {
-    if (this.existingAdmin) {
-      return;
+    // If not, create the user.
+    if (!this.existingAdmin) {
+      this.createdAdmin = await new User({
+        active: true,
+        roles: [ 'admin' ],
+        email: this.adminEmail,
+        password: this.adminPassword
+      }).save();
     }
-
-    return new User({
-      active: true,
-      roles: [ 'admin' ],
-      email: this.adminEmail,
-      password: this.adminPassword
-    }).save().then(admin => {
-      this.createdAdmin = admin;
-    });
   }
 
   onSuccess() {
